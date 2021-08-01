69fb0a06d92aa6f437913148339c832d5ddaecfe
WangYeWei <826036140@qq.com>
2021-08-01 17:03:22 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,12 @@ import Button, { ButtonType, ButtonSize } from './components/Button/Button'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button disabled>Hello</Button>
-
-        <Button btnType={ButtonType.Primary}
-          size={ButtonSize.Large}
-        >
-          Hello
-        </Button>
-
-        <Button btnType={ButtonType.Link}
-          href="https://www.baidu.com"
-        >
-          Baidu Link
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>button</Button>
+      <Button disabled>disabled</Button>
+      <Button size={ButtonSize.Large} btnType={ButtonType.Primary}>Large Primary</Button>
+      <Button size={ButtonSize.Small} btnType={ButtonType.Danger}>small danger</Button>
+      <Button btnType={ButtonType.Link} href="https://www.baidu.com">Link</Button>
+      <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled>Link Disabled</Button>
     </div>
   );
 }
