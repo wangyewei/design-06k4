c0ba0437d7b56c9b1de0ead2ee9dd6b6f828274c
WangYeWei <826036140@qq.com>
2021-08-04 21:08:54 +0800

    storybook of button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -8,5 +8,20 @@ import Button from './Button'
 const defaultButton = () => (
   <Button onClick={action('clicked')}>default button</Button>
 )
+const diffrentSizeButton = () => (
+  <>
+    <Button size="lg">A Large Button</Button>
+    <Button size="sm">A Small Button</Button>
+  </>
+)
 
-storiesOf('Button Component', module).add('默认 button', defaultButton)
+const diffrentTypeButton = () => (
+  <>
+    <Button btnType="danger">A danger Button</Button>
+    <Button btnType="primary">A primary Button</Button>
+    <Button btnType="default">A default Button</Button>
+    <Button btnType="link" href="https://github/wangyewei">A link Button</Button>
+  </>
+)
+
+storiesOf('Button Component', module).add('默认 button', defaultButton).add('不同尺寸 button', diffrentSizeButton).add('不同类型 button', diffrentTypeButton)
