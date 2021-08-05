f7d098cc8198e818dd5dd124d1635add9b08eaa4
WangYeWei <826036140@qq.com>
2021-08-05 15:17:55 +0800

    addons

---
+++ .storybook/config.tsx
@@ -0,0 +1,13 @@
+import { configure, addDecorator } from '@storybook/react';
+import React from 'react';
+import "../src/styles/index.scss"
+const styles: React.CSSProperties = {
+  textAlign: 'center',
+  margin: '10px'
+}
+
+const centerDecorator = (storyFn: any) => <div style={styles}>{storyFn()}</div>
+
+addDecorator(centerDecorator)
+
+configure(require.context('../src/components', true, /\.stories\.tsx$/), module)
