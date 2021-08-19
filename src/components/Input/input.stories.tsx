3521337c44e180a692329d8f8dcf7ea5ce61ec43
WangYeWei <826036140@qq.com>
2021-08-20 01:32:30 +0800

    different size input

--- src/components/Input/input.stories.tsx
+++ src/components/Input/input.stories.tsx
@@ -14,6 +14,18 @@ const disabledInput = () => (
   <KInput style={{ width: '300px' }} placeholder="被禁用的input" disabled />
 )
 
+const diffrentSizeInput = () => (
+  <div style={{ width: '430px' }}>
+    <KInput
+      defaultValue="大号的输入框"
+      size="lg" />
+    <KInput
+      defaultValue="小号的输入框"
+      size="sm" />
+    <KInput defaultValue="默认的输入框" />
+  </div>
+)
+
 const iconInput = () => (
   <KInput
     style={{ width: '300px' }}
@@ -22,7 +34,7 @@ const iconInput = () => (
 )
 
 const apendInput = () => (
-  <div style={{ width: "500px" }}>
+  <div style={{ width: "430px" }}>
     <p>带前缀</p>
     <KInput
       prepend="https://" />
@@ -34,6 +46,7 @@ const apendInput = () => (
 
 storiesOf('输入框 KIpunt', module)
   .add('KInput', defaultInput)
+  .add('不同尺寸的输入框', diffrentSizeInput)
   .add('被禁用的输入框', disabledInput)
   .add('带icon图标的输入框', iconInput)
   .add("带前后缀的Input", apendInput)
