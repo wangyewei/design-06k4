22163a2d0de7b22d38e98d2833d624a1e553a424
WangYeWei <826036140@qq.com>
2022-03-07 16:51:17 +0800

    update: button type primary style

---
+++ src/packages/button/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: YeWei Wang
+ * @Date: 2022-03-07 16:17:45
+ * @WeChat: wj826036
+ * @Motto: 求知若渴，虚心若愚
+ * @Description: 出口
+ * @LastEditTime: 2022-03-07 16:17:45
+ * @Version: 1.0
+ * @FilePath: \design-06k4\src\packages\button\index.ts
+ */
+
+export type { ButtonBaseProps } from './Button'
+export { KButton } from './Button'
