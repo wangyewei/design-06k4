881df961ce76e376d1ea6f0a8d94b683bbd1a3e3
WangYeWei <826036140@qq.com>
2022-05-22 20:05:39 +0800

    update:(v2.15.1)add input size and prefix icon

--- src/packages/input/Input.tsx
+++ src/packages/input/Input.tsx
@@ -1,24 +1,32 @@
 import React, { Component, CSSProperties, FC, InputHTMLAttributes } from "react";
 import { getPrefixCls } from "@/utils";
 import classNames from "classnames";
+import KIcon, { IconProps } from "../icon";
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
   className?: string,
   style?: CSSProperties,
+  prefixIcon?: IconProps['icon'],
+  size?: 'small' | 'middle' | 'large'
 }
 
 const RowInput: FC<InputProps> = props => {
 
-  const { className, style, ...restProps } = props
+  const { className, style, prefixIcon, size = 'middle', ...restProps } = props
 
   const prefixCls = getPrefixCls('input')
 
   const cnames = classNames(
     prefixCls,
+    {
+      [`${prefixCls}-with-prefix`]: prefixIcon,
+      [`${prefixCls}-${size}`]: size
+    },
     className
   )
   return (
     <div className={cnames} style={{ ...style }} >
+      {prefixCls && <KIcon icon={prefixIcon} className={`${prefixCls}-prefix`} />}
       <input type="text" className={`${prefixCls}-inner`} {...restProps} />
     </div>
   )
--- src/packages/input/__example__/Baisc-input.tsx
+++ src/packages/input/__example__/Baisc-input.tsx
@@ -2,5 +2,23 @@ import React from "react";
 import KInput from "../Input";
 
 export default () => (
-  <KInput placeholder="placeholder" style={{ width: '350px', margin: '12px' }} />
+  <>
+    <br />
+    <KInput placeholder="placeholder"
+      style={{ width: '350px', margin: '12px' }} />
+    <br />
+    <KInput placeholder="large input"
+      prefixIcon="user"
+      size="large"
+      style={{ width: '350px', margin: '12px' }} />
+    <KInput
+      placeholder="middle input"
+      prefixIcon="user"
+      style={{ width: '350px', margin: '12px' }} />
+    <KInput
+      placeholder="small input"
+      prefixIcon="user"
+      size="small"
+      style={{ width: '350px', margin: '12px' }} />
+  </>
 )
