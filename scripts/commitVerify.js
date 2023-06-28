483a6a8efdfb0210e8950084963636aae2c4275c
WangYeWei <826036140@qq.com>
2023-06-28 12:22:39 +0800

    ci: verify git commit

---
+++ scripts/commitVerify.js
@@ -0,0 +1,32 @@
+/**
+ * copy from vue
+ * @url https://github.com/vuejs/core/blob/main/scripts/verifyCommit.js
+ */
+import chalk from "chalk"
+import * as fs from "fs"
+import path from "path"
+
+const readFileSync = fs.readFileSync
+const msgPath = path.resolve(".git/COMMIT_EDITMSG")
+const msg = readFileSync(msgPath, "utf-8").trim()
+
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
+      `invalid commit message format`
+    )}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
+      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${chalk.green(
+        `fix(v-model): handle events on blur (close #28)`
+      )}\n\n` +
+      chalk.red(`  See .github/commit-convention.md for more details.\n`)
+  )
+  process.exit(1)
+}
