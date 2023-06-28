22c1a8d02804e68c6869a12cba950d9e9a9db956
WangYeWei <826036140@qq.com>
2023-06-28 11:51:33 +0800

    style: code prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,31 @@
-import * as path from 'path'
-import * as fs from 'fs'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from 'rollup-plugin-typescript2'
-import postcss from 'rollup-plugin-postcss'
-import json from '@rollup/plugin-json'
-import babel from '@rollup/plugin-babel'
-import sass from 'sass'
+import * as path from "path"
+import * as fs from "fs"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "rollup-plugin-typescript2"
+import postcss from "rollup-plugin-postcss"
+import json from "@rollup/plugin-json"
+import babel from "@rollup/plugin-babel"
+import sass from "sass"
 
-// 入口
-
-const entry = 'components/index.ts'
-const componentsDir = 'components/packages'
+/**entries */
+const entry = "components/index.ts"
+const componentsDir = "components/packages"
 const componentsName = fs.readdirSync(path.resolve(componentsDir))
 const componentsEntry = componentsName.map(
   (name) => `${componentsDir}/${name}/index.ts`
 )
 
-// 环境变量
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production"
 const BABEL_ENV = process.env.BABEL_ENV
 
-// Babel配置
 const babelOptions = {
   presets: ["@babel/preset-env"],
-  extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],
-  exclude: "**/node_modules/**"
+  extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+  exclude: "**/node_modules/**",
 }
 
-// 通用插件
 const commonPlugins = [
   peerDepsExternal(),
   resolve(),
@@ -37,66 +33,52 @@ const commonPlugins = [
   typescript(),
   babel(babelOptions),
   json(),
-  // dts()
 ]
 
-// 忽略文件
 const externalConfig = [
-  id => /\/__expample__|main.tsx/.test(id),
-  'react',
-  'react-dom',
-  'classname',
-  'react-is',
+  (id) => /\/__expample__|main.tsx/.test(id),
+  "react",
+  "react-dom",
+  "classname",
+  "react-is",
   "@fortawesome/fontawesome-svg-core",
   "@fortawesome/free-solid-svg-icons",
   "@fortawesome/react-fontawesome",
-  '**/node_modules/**'
+  "**/node_modules/**",
 ]
 
-// sass打包
 const processScss = function (context) {
   return new Promise((resolve, reject) => {
-    sass.compile(
-      {
-        file: context
-      },
-      function (err, result) {
-        if (!err) {
-          resolve(result)
-        } else {
-          reject(result)
-        }
-      }
-    )
+    sass.compile({ file: context }, (err, result) => {
+      err ? reject(result) : resolve(reject)
+    })
   })
 }
-
 
 const esmOutput = {
   preserveModules: true,
 }
 
+function esmConfig() {
+  return {
+    input: [entry, ...componentsEntry],
+    output: { ...esmOutput, dir: "dist/", format: "es" },
+    external: externalConfig,
+    plugins: [
+      postcss({
+        extract: true,
+        process: processScss,
+      }),
+      ...commonPlugins,
+    ],
+  }
+}
+
 export default () => {
   switch (BABEL_ENV) {
-    case 'esm':
-      return {
-        input: [entry, ...componentsEntry],
-        output: { ...esmOutput, dir: 'dist/', format: 'es' },
-        external: externalConfig,
-        plugins: [postcss({
-          extract: true,
-          process: processScss,
-        }), ...commonPlugins]
-      }
+    case "esm":
+      return esmConfig()
     default:
-      return {
-        input: [entry, ...componentsEntry],
-        output: { ...esmOutput, dir: 'dist/', format: 'es' },
-        external: externalConfig,
-        plugins: [postcss({
-          extract: true,
-          process: processScss,
-        }), ...commonPlugins]
-      }
+      return esmConfig()
   }
 }
--- scripts/build-css.js
+++ scripts/build-css.js
@@ -1,24 +1,24 @@
-import * as fs from 'fs';
-import { execa } from 'execa';
-import path from 'path';
+import * as fs from "fs"
+import { execa } from "execa"
+import path from "path"
 
 /**
  * The directory where the components are located.
  * @type {string}
  */
-const componentsDir = 'components/packages';
+const componentsDir = "components/packages"
 
 /**
  * The output directory for compiled CSS.
  * @type {string}
  */
-const outputDir = 'dist/packages';
+const outputDir = "dist/packages"
 
 /**
  * Get the list of component names from the components directory.
  * @type {string[]}
  */
-const componentNames = fs.readdirSync(componentsDir);
+const componentNames = fs.readdirSync(componentsDir)
 
 /**
  * Compile Sass to CSS using the `sass` command.
@@ -28,13 +28,15 @@ const componentNames = fs.readdirSync(componentsDir);
  */
 const compileSass = async (sassFilePath, cssFilePath) => {
   try {
-    await execa('sass', [sassFilePath, cssFilePath, '--no-source-map'], { stdio: 'inherit' });
-    console.log(`Successfully compiled Sass: ${sassFilePath}`);
+    await execa("sass", [sassFilePath, cssFilePath, "--no-source-map"], {
+      stdio: "inherit",
+    })
+    console.log(`Successfully compiled Sass: ${sassFilePath}`)
   } catch (err) {
-    console.error(`Error compiling Sass: ${sassFilePath}`);
-    console.error(err);
+    console.error(`Error compiling Sass: ${sassFilePath}`)
+    console.error(err)
   }
-};
+}
 
 /**
  * Compile Sass for all components.
@@ -42,28 +44,28 @@ const compileSass = async (sassFilePath, cssFilePath) => {
  */
 const compileComponents = async () => {
   const compileTasks = componentNames.map(async (componentName) => {
-    const componentDir = path.join(componentsDir, componentName);
-    const sassFilePath = path.join(componentDir, 'styles', '_index.scss');
-    const outputStyleDir = path.join(outputDir, componentName, 'style');
-    const cssFilePath = path.join(outputStyleDir, 'index.css');
+    const componentDir = path.join(componentsDir, componentName)
+    const sassFilePath = path.join(componentDir, "styles", "_index.scss")
+    const outputStyleDir = path.join(outputDir, componentName, "style")
+    const cssFilePath = path.join(outputStyleDir, "index.css")
 
     if (fs.existsSync(sassFilePath)) {
       // Create the output directory
-      fs.mkdirSync(outputStyleDir, { recursive: true });
+      fs.mkdirSync(outputStyleDir, { recursive: true })
 
-      await compileSass(sassFilePath, cssFilePath);
+      await compileSass(sassFilePath, cssFilePath)
     } else {
-      console.warn(`Sass file not found: ${sassFilePath}`);
+      console.warn(`Sass file not found: ${sassFilePath}`)
     }
-  });
+  })
 
   try {
-    await Promise.all(compileTasks);
-    console.log('All components compiled successfully.');
+    await Promise.all(compileTasks)
+    console.log("All components compiled successfully.")
   } catch (err) {
-    console.error('Error compiling components:');
-    console.error(err);
+    console.error("Error compiling components:")
+    console.error(err)
   }
-};
+}
 
-compileComponents();
+compileComponents()
